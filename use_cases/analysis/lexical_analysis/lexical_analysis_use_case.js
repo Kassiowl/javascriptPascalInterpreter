4ea05efc49b7bf11a6d659bff513728ccfa1c3ab
Kassio Wallace <kassiowl34@gmail.com>
2023-04-22 14:30:12 -0300

    separate words fix

--- use_cases/analysis/lexical_analysis/lexical_analysis_use_case.js
+++ use_cases/analysis/lexical_analysis/lexical_analysis_use_case.js
@@ -1,6 +1,4 @@
 const Token = require("../../../entities/token")
-const ErrorHandling = require("../error_handling/error_handling")
-
 class LexicalAnalysis{
     constructor(expression)
     {
@@ -8,12 +6,28 @@ class LexicalAnalysis{
     }
 
     separateWords() {
-        const words = this.expression.split(/[^a-zA-Z0-9=]+/);
-      
-        const filteredWords = words.filter(word => word !== '');
-    
-        return filteredWords;
+      const words = this.expression.split(/([^a-zA-Z0-9=])/).filter(word => word !== '');
+      let bracketOpened = false;
+      let filteredWords = [];
+  
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+  
+        if (word === "{") {
+          bracketOpened = true;
+          filteredWords.push(word);
+        } else if (word === "}") {
+          bracketOpened = false;
+          filteredWords.push(word);
+        } else if (bracketOpened) {
+          filteredWords.push(word);
+        } else {
+          filteredWords.push(word.replace(/\s/g, ""));
+        }
       }
+  
+      return filteredWords;
+    }
     run()
     {
         let word = this.separateWords(this.expression)
@@ -28,5 +42,5 @@ class LexicalAnalysis{
 
 
 // teste
-lexical = new LexicalAnalysis("const X = 20")
+lexical = new LexicalAnalysis("const X = {20} // {}")
 console.log(lexical.run())
