9db5b5dd18c67a98c79282539e45fbc9bd0a33e4
Kassio Wallace <kassiowl34@gmail.com>
2023-06-05 15:12:28 -0300

    informal interface add

---
+++ PascalJavascriptInterpreter/LexicalAnalysisHashTable/interface.js
@@ -0,0 +1,11 @@
+class token {
+    constructor(token_list) {
+      this.token_list = token_list
+    }
+
+    buildHashTable(){}
+
+    run(){
+        return this.buildHashTable()
+    }
+  }
---
+++ PascalJavascriptInterpreter/Token_Tree/interface.js
@@ -0,0 +1,11 @@
+class tokenTreeInterface {
+    constructor(token_list) {
+      this.token_list = token_list
+    }
+
+    buildTree(){}
+
+    run(){
+        return this.buildTree()
+    }
+  }
