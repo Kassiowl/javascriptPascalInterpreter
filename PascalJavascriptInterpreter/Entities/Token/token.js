5389be0963ece134dbda72a650f98abc9f2765e4
Kassio Wallace <kassiowl34@gmail.com>
2023-06-12 18:37:50 -0300

    token modd

--- PascalJavascriptInterpreter/Entities/Token/token.js
+++ PascalJavascriptInterpreter/Entities/Token/token.js
@@ -2,13 +2,7 @@
 module.exports = class Token {
     constructor(token_data) {
       this.token_data = token_data;
-      this.token_specify = this.get_specify_token()
-    }
-
-    get_specify_token(){
-
-        let token_specify = ''
-        return token_specify
+      this.token_specify = null
     }
 }
 
